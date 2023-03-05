6f0c53a2b5f7af27cb76e92b73be8e70e9c6e46b
ryhhill1998 <ryhhill1998@outlook.com>
2023-03-05 08:45:27 +0000

    Added basic form design to bulletin board

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ const App = () => {
     return (
         <div className="App">
             <h1>Bulletin Board</h1>
+
+            <form>
+                <h2>Add new post</h2>
+
+                <label>
+                    Post title
+                    <input type="text" required />
+                </label>
+
+                <label>
+                    Author
+                    <input type="text" required />
+                </label>
+
+                <label>
+                    Post title
+                    <textarea rows="5" required />
+                </label>
+
+                <button>Save post</button>
+            </form>
         </div>
     );
 }
